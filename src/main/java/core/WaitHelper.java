2a1f9163f104a011da1bb4c2efa870582f0af66a
Minas Ghazaryan <minas2712@gmail.com>
2024-05-13 18:14:46 +0400

    Wait helper class setup

--- src/main/java/core/WaitHelper.java
+++ src/main/java/core/WaitHelper.java
@@ -1,11 +1,40 @@
 package core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class WaitHelper {
-    private WebDriver driver;
+    private  WebDriver driver;
 
-    public WaitHelper(WebDriver driver){
+    public WaitHelper(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void waitUntilVisibility(WebElement element, int sec){
+        new WebDriverWait(driver, Duration.ofSeconds(sec))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitUntilVisibility(WebElement element){
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public  void waitUntilClickable(WebElement element, int sec){
+        new WebDriverWait(driver, Duration.ofSeconds(sec))
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public  void waitUntilClickable(WebElement element){
+        new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void pause(int sec) throws InterruptedException {
+        Thread.sleep(sec * 1000);
     }
 }
